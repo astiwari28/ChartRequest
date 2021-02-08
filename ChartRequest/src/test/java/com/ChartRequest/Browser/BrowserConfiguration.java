04764e90057b19b23c81f592418a3b3479362f6c
Ashutosh Tiwari <71018794+astiwari28@users.noreply.github.com>
2021-02-08 14:19:40 +0530

    Try with chrome binary file

--- ChartRequest/src/test/java/com/ChartRequest/Browser/BrowserConfiguration.java
+++ ChartRequest/src/test/java/com/ChartRequest/Browser/BrowserConfiguration.java
@@ -34,11 +34,11 @@ public class BrowserConfiguration {
 	}
 
 	public void browserTearUp(String URL) {
-		WebDriverManager.chromedriver().setup();
-		ChromeOptions options=new ChromeOptions();
-		options.addArguments("--disable-geolocation");
-		//System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "/src/test/resources/DriversExe/chromedriver.exe");
-		driver = new ChromeDriver(options);
+		//WebDriverManager.chromedriver().setup();
+		/*ChromeOptions options=new ChromeOptions();
+		options.addArguments("--disable-geolocation");*/
+		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "/src/test/resources/DriversExe/chromedriver_binary");
+		driver = new ChromeDriver();
 		//driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
