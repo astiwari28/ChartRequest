09ba894bd3c45a284b4600d23c63ed244a4d4902
Ashutosh Tiwari <71018794+astiwari28@users.noreply.github.com>
2021-02-08 18:47:14 +0530

    Adding new Headless Options

--- ChartRequest/src/test/java/com/ChartRequest/Browser/BrowserConfiguration.java
+++ ChartRequest/src/test/java/com/ChartRequest/Browser/BrowserConfiguration.java
@@ -36,7 +36,15 @@ public class BrowserConfiguration {
 	public void browserTearUp(String URL) {
 		WebDriverManager.chromedriver().setup();
 		ChromeOptions options=new ChromeOptions();
-		options.addArguments("--headless");
+		options.addArguments("start-maximized"); // https://stackoverflow.com/a/26283818/1689770
+		options.addArguments("enable-automation"); // https://stackoverflow.com/a/43840128/1689770
+		options.addArguments("--headless"); // only if you are ACTUALLY running headless
+		options.addArguments("--no-sandbox"); //https://stackoverflow.com/a/50725918/1689770
+		options.addArguments("--disable-infobars"); //https://stackoverflow.com/a/43840128/1689770
+		options.addArguments("--disable-dev-shm-usage"); //https://stackoverflow.com/a/50725918/1689770
+		options.addArguments("--disable-browser-side-navigation"); //https://stackoverflow.com/a/49123152/1689770
+		options.addArguments("--disable-gpu"); //https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
+		driver = new ChromeDriver(options);
 		//System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "/src/test/resources/DriversExe/chromedriver_binary");
 		driver = new ChromeDriver(options);
 		//driver = new FirefoxDriver();
