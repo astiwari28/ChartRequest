f7d6b47701e34bed5349c435f1b7b0537da958fc
Ashutosh Tiwari <71018794+astiwari28@users.noreply.github.com>
2021-02-09 12:13:07 +0530

    Reset The Code

--- ChartRequest/src/test/java/com/ChartRequest/Browser/BrowserConfiguration.java
+++ ChartRequest/src/test/java/com/ChartRequest/Browser/BrowserConfiguration.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 
-import com.ChartRequest.Reports.TestListener;
 import com.ChartRequest.Utility.TestUtil;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -34,27 +33,30 @@ public class BrowserConfiguration {
 	}
 
 	public void browserTearUp(String URL) {
-		WebDriverManager.chromedriver().setup();
-		ChromeOptions options=new ChromeOptions();
-		options.addArguments("start-maximized"); // https://stackoverflow.com/a/26283818/1689770
-		options.addArguments("enable-automation"); // https://stackoverflow.com/a/43840128/1689770
-		options.addArguments("--headless"); // only if you are ACTUALLY running headless
-		options.addArguments("--no-sandbox"); //https://stackoverflow.com/a/50725918/1689770
-		options.addArguments("--disable-infobars"); //https://stackoverflow.com/a/43840128/1689770
-		options.addArguments("--disable-dev-shm-usage"); //https://stackoverflow.com/a/50725918/1689770
-		options.addArguments("--disable-browser-side-navigation"); //https://stackoverflow.com/a/49123152/1689770
-		options.addArguments("--disable-gpu"); //https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
-		driver = new ChromeDriver(options);
-		//System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "/src/test/resources/DriversExe/chromedriver_binary");
-		driver = new ChromeDriver(options);
-		//driver = new FirefoxDriver();
-		driver.manage().window().maximize();
-		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
-		driver.get(URL);
-		System.out.println("URL :- "+URL);
-		//TestListener.infoMessage("URL :- "+URL);
+		try {
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions options=new ChromeOptions();
+			options.addArguments("--start-maximized"); 
+			options.addArguments("--enable-automation");
+			options.addArguments("--headless"); 
+			options.addArguments("--no-sandbox"); //https://stackoverflow.com/a/50725918/1689770
+			options.addArguments("--disable-infobars"); //https://stackoverflow.com/a/43840128/1689770
+			options.addArguments("--disable-dev-shm-usage"); //https://stackoverflow.com/a/50725918/1689770
+			options.addArguments("--disable-browser-side-navigation"); //https://stackoverflow.com/a/49123152/1689770
+			options.addArguments("--disable-gpu");
+			//System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "/src/test/resources/DriversExe/chromedriver_binary");
+			driver = new ChromeDriver(options);
+			//driver = new FirefoxDriver();
+			driver.manage().window().maximize();
+			driver.manage().deleteAllCookies();
+			driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+			driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+			driver.get(URL);
+			System.out.println("URL :- "+URL);
+			//TestListener.infoMessage("URL :- "+URL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@AfterMethod(enabled = true)
